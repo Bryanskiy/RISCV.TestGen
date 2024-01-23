f16cc4a5ed6a91f35cbe6612b936ac3787ba6ab8
uslstenn <shurygin0171@gmail.com>
2024-01-23 18:51:16 +0300

    fix

--- generator.py
+++ generator.py
@@ -23,6 +23,7 @@ ASM_PREAMBULE = textwrap.dedent(
     """
 )
 
+@dataclass
 class Generator:
     seed: int = 0
     adr_range: ClassVar[tuple[int, int]] = (0x10, 0x4000)
@@ -65,7 +66,8 @@ class Generator:
 
         def gen_preambula():
             return [
-                instr.Instruction(isa.info.InstrNameTy.LUI, rd=base, imm=addr_regval),
+                instr.Instruction(isa.info.InstrNameTy.LUI,
+                                  rd=base, imm=addr_regval),
                 instr.Instruction(
                     isa.info.InstrNameTy.ADDI, rd=base, imm=addr_regval, rs1=0
                 ),
@@ -114,87 +116,6 @@ class Generator:
         return list(
             map(
                 lambda _: self.generateInstr(instr_set),
-                range(instrs_count),
-            )
-        )
-
-    @staticmethod
-    def __randgen(bound: int):
-        return random.randint(0, bound - 1)
-
-    @staticmethod
-    def __regGen():
-        return Generator.__randgen(1 << 5)
-
-    def generateInstr(self, allowed: Sequence[isa.info.InstrNameTy]):
-        random.seed(self.seed)
-        name = isa.info.InstrNameTy(random.choice(allowed))
-
-        return self.generateInstrNamed(name)
-
-    def generateMem(self, count: int = 10):
-        addr_regval = random.randint(self.adr_range[0], self.adr_range[1])
-        addr_offset = random.randint(
-            0,
-            min(
-                max(abs(self.adr_range[i] - addr_regval) for i in range(2)),
-                (1 << 12) - 1,
-            ),
-        )
-
-        base = self.__regGen()
-
-        def gen_preambula():
-            return [
-                instr.Instruction(isa.info.InstrNameTy.LUI, rd=base, imm=addr_regval),
-                instr.Instruction(
-                    isa.info.InstrNameTy.ADDI, rd=base, imm=addr_regval, rs1=0
-                ),
-            ]
-
-        instr_stream = (
-            gen_preambula()
-            + [
-                instr.Instruction(
-                    isa.info.InstrNameTy.SB,
-                    rs2=self.__regGen(),
-                    rs1=base,
-                    imm=addr_offset,
-                ),
-            ]
-            + self.generateBB()
-            + gen_preambula()
-            + [
-                instr.Instruction(
-                    isa.info.InstrNameTy.LB,
-                    rd=self.__regGen(),
-                    rs1=base,
-                    imm=addr_offset,
-                ),
-            ]
-        )
-
-        return instr_stream
-
-    def generateBB(self, instrs_count: int = 10) -> list[instr.Instruction]:
-        instr_set = list(isa.info.InstrNameTy)
-        for name, fmt in isa.info.NAME_TO_FORMAT.items():
-            if fmt in (isa.info.InstrFormatTy.J, isa.info.InstrFormatTy.B):
-                instr_set.remove(name)
-
-        ignored_categories = [
-            isa.info.InstrCategoryTy.SYS,
-            isa.info.InstrCategoryTy.LOAD,
-            isa.info.InstrCategoryTy.STORE,
-        ]
-
-        for instr_category in ignored_categories:
-            for name in isa.info.CATEGORY_TO_INSTR[instr_category]:
-                instr_set.remove(name)
-
-        return list(
-            map(
-                lambda _: random.choice(instr_set),
                 range(instrs_count),
             )
         )
